62676a620e185dcb61fe3301364861304394ddfa
André Vaccari <andrevaccari95@gmail.com>
2018-03-14 11:46:54 -0300

    React with parcel boilerplate implemented
    Some initial configs

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import 'src/index.scss'
+
+const App = () =>
+  <div>
+    Blog!
+  </div>
+
+ReactDOM.render(<App />, document.getElementById('app'))
